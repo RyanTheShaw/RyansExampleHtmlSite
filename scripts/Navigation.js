597c8c1541dc2bee20830520d795d8c90e48e836
RyanTheShaw <numberonecharizard@gmail.com>
2024-11-04 15:28:28 -0500

    Additional javascript behavior

--- scripts/Navigation.js
+++ scripts/Navigation.js
@@ -1,3 +1,9 @@
 function openFoodWebsite() {
     window.open("https://www.allrecipes.com", "_blank");
 }
+
+function incrementSmoothieCount() {
+    const counterElement = document.getElementById("smoothie-counter");
+    let currentCount = parseInt(counterElement.textContent, 10);
+    counterElement.textContent = currentCount + 1;
+}
